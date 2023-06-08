64739ae211cdda48bfc86ac0b9bd912ea9b5eed1
PureCyan <134799725+PureCyan@users.noreply.github.com>
2023-06-08 14:43:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ browser = login("https://maozhua.org/", args.pd, args.acc)
 # 签到
 #sign(browser, "https://maozhua.org/mission/today")
 
-# 评论
-links = get_links("https://maozhua.org/tags")
-random_link = random_links(links)
-for element in random_link:
-  comment(browser, element)
+# 评论(需要修改)
+# links = get_links("https://maozhua.org/tags")
+# random_link = random_links(links)
+# for element in random_link:
+#   comment(browser, element)

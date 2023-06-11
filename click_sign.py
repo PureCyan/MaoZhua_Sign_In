463c485ea2cf2ab8cb35899d656196ae2a860e9e
PureCyan <134799725+PureCyan@users.noreply.github.com>
2023-06-11 11:25:42 +0800

    Update click_sign.py

--- click_sign.py
+++ click_sign.py
@@ -3,5 +3,5 @@ import time
 
 def sign(browser, web):
     browser.get(web)
-    time.sleep(8)
+    time.sleep(10)
     browser.find_element(By.XPATH, "/html/body/div[1]/div[2]/div[1]/div/main/div[1]/div[2]/div[2]/button").click()
